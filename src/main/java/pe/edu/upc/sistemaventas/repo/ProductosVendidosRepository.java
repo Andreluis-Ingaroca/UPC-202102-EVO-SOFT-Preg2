8d3025ec6963e18adf836c8045a0a35e8d9f2927
Andreluis Ingaroca Torres <andreluispit@gmail.com>
2021-12-02 18:00:24 -0500

    Final Commit

--- src/main/java/pe/edu/upc/sistemaventas/repo/ProductosVendidosRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.sistemaventas.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pe.edu.upc.sistemaventas.model.ProductoVendido;
-
-public interface ProductosVendidosRepository extends CrudRepository<ProductoVendido, Integer> {
-
-}
--- src/main/java/pe/edu/upc/sistemaventas/repo/VentasRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.sistemaventas.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pe.edu.upc.sistemaventas.model.Venta;
-
-public interface VentasRepository extends CrudRepository<Venta, Integer> {
-}
